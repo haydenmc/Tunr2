6b0b2a2a30092dec95ba355a1105f0ab233caa72
Hayden McAfee <hayden@outlook.com>
2017-01-09 17:38:01 -0800

    Started some react stuffs.

---
+++ Client/Components/Tunr.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export interface TunrProps { }
+
+export class Tunr extends React.Component<TunrProps, undefined> {
+    public render() {
+        return <h1>It works!</h1>;
+    }
+}
